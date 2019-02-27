55579327c0effd8d2ca4ec504ed401613007a8d1
Anh Nguyen <anh.nguynngctruc@gmail.com>
2019-02-27 22:29:01 +0200

    fetch joke api

--- Practising/joke-api/src/App.js
+++ Practising/joke-api/src/App.js
@@ -10,10 +10,16 @@ class App extends Component {
       category: ""
     };
   }
-
+  inputChange = event => {
+    event.preventDefault();
+    this.setState({ category: event.target.value });
+  };
   next = event => {
     event.preventDefault();
-    fetch("http://api.icndb.com/jokes/random")
+
+    fetch(
+      "http://api.icndb.com/jokes/random?limitTo=[" + this.state.category + "]"
+    )
       .then(response => response.json())
       .then(responseData => {
         console.log(responseData);
@@ -26,8 +32,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="text"
+          onChange={this.inputChange}
+          value={this.state.category}
+        />
         <button onClick={this.next}>Wanna more joke?</button>
-        <p>{this.state.category}</p>
+        <p>
+          {this.state.category != "nerdy" && this.state.category != "explicit"
+            ? "There is no such category"
+            : this.state.category}
+        </p>
         <p>{this.state.joke}</p>
       </div>
     );
